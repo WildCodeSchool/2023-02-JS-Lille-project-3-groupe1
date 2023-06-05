7690b04b30b05d314b5a370fe395957d87e5dcf1
KriZpAlex <a.rohde.ferreira@gmail.com>
2023-06-05 17:26:08 +0200

    Variables Sass

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.scss";
+export default function App() {
+  return (
+    <div>
+      <h1>Tableaux 1.</h1>
+      <h2>Tableaux 1.</h2>
+      <h3>Tableaux 1.</h3>
+      <h4>Tableaux 1.</h4>
+    </div>
+  );
+}
