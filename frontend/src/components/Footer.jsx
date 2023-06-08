12896edd93dcf69a12c5fc6d932489c9fe6c8b46
KriZpAlex <a.rohde.ferreira@gmail.com>
2023-06-08 16:35:01 +0200

    Footer Done: Logo Links Added, Txt's to the same dimensions

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,15 +1,23 @@
+import { Link } from "react-router-dom";
 import Logo1 from "../assets/ObjetTemoin/Logos - Objet Témoin/PNG/Logo1.png";
 import Wild from "../assets/ObjetTemoin/Logos - Objet Témoin/PNG/Wild.png";
+
 export default function Footer() {
   return (
     <div className="Footer">
       <div className="ImgObj">
-        {" "}
-        {<img className="ImgLog" src={Logo1} alt="Logo1"></img>}
+        <Link to="https://museo.vandanjon.com/index.php/en/">
+          <img className="ImgLog" src={Logo1} alt="Logo1" />
+        </Link>
+
         <p>Remerciement à notre partenaire Objet Témoin.</p>
       </div>
+
       <div className="ImgWild">
-        {<img className="ImgLog2" src={Wild} alt="Wild"></img>}
+        <Link to="https://www.wildcodeschool.com/fr-FR">
+          <img className="ImgLog2" src={Wild} alt="Wild" />
+        </Link>
+
         <p>Projet en collaboration avec la Wild Code School.</p>
       </div>
     </div>
