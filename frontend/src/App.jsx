6973de820769f136f04aa7fe2e1a1759682c591d
hugocoding21 <118363369+hugocoding21@users.noreply.github.com>
2023-06-08 16:53:04 +0200

    start home page desktop

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,14 @@
 import "./App.scss";
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./assets/components/Navbar/Navbar";
+import Home from "./assets/components/Home/Home";
 
 export default function App() {
   return (
     <>
       <Navbar />
-
       <Routes>
-        <Route path="/" element="" />
+        <Route path="/" element={<Home />} />
         <Route path="/galerie" element="" />
         <Route path="/apropos" element="" />
       </Routes>
---
+++ frontend/src/assets/components/Home/Home.jsx
@@ -0,0 +1,44 @@
+import { NavLink } from "react-router-dom";
+import logohome from "../../logos/logohome.svg";
+import "./Home.scss";
+
+export default function Home() {
+  return (
+    <div className="containerhome">
+      <div className="afca">
+        <div className="imghome">
+          <img className="logohome" src={logohome} alt="logo afca" />
+        </div>
+
+        <div className="text">
+          <p>
+            L’Association des Familles Amies de Capeline 974 souhaite mettre à
+            disposition des élèves de l’Océan Indien et de leurs familles des
+            ressources spécifiques aux espaces ultramarins.
+          </p>
+          <p>
+            Plongez au cœur de la magie artistique lors de notre exposition
+            exclusive dédiée aux œuvres de l'artiste renommé Hippolyte Mortier
+            de Trévise. Avec une virtuosité exceptionnelle, Mortier de Trévise
+            donne vie à ses dessins et aquarelles, révélant un monde empreint de
+            beauté et d'émotion. Ses dessins, d'une précision remarquable,
+            captent chaque détail avec une habileté remarquable, tandis que ses
+            aquarelles transmettent une douceur et une délicatesse qui invitent
+            à l'émerveillement.
+          </p>
+        </div>
+      </div>
+
+      <NavLink to="/galerie" className="galerie">
+        Voir les œuvres
+      </NavLink>
+      <div>
+        <span className="sliderart1" />
+      </div>
+
+      <div>
+        <span className="sliderart1" />
+      </div>
+    </div>
+  );
+}
