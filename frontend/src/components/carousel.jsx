8c39628c054c3b3215a68d7d9d0f682570829e38
Michael_McGrath <corecreate0@gmail.com>
2023-06-08 19:39:57 +0200

    carousel added + moved footer

--- frontend/src/components/carousel.jsx
+++ frontend/src/components/carousel.jsx
@@ -1,17 +1,16 @@
+/* eslint-disable import/no-unresolved */
 import React, { useState } from "react";
 // Import Swiper React components
-import { Swiper, SwiperSlide } from "swiper/react";
-
-// Import Swiper styles
-import "swiper/scss";
+import "swiper/scss"; // eslint-disable-line
+// import "swiper/react";
 import "swiper/scss/free-mode";
 import "swiper/scss/navigation";
 import "swiper/scss/thumbs";
-
 import "./carousel.scss";
+import { Swiper, SwiperSlide } from "swiper/react"; // eslint-disable-line
 
 // import required modules
-import { FreeMode, Navigation, Thumbs } from "swiper";
+import { FreeMode, Navigation, Thumbs } from "swiper"; // eslint-disable-line
 
 export default function Carousel() {
   const [thumbsSwiper, setThumbsSwiper] = useState(null);
@@ -23,9 +22,9 @@ export default function Carousel() {
           "--swiper-navigation-color": "#fff",
           "--swiper-pagination-color": "#fff",
         }}
-        loop={true}
+        loop
         spaceBetween={10}
-        navigation={true}
+        navigation
         thumbs={{
           swiper: thumbsSwiper && !thumbsSwiper.destroyed ? thumbsSwiper : null,
         }}
@@ -105,60 +104,70 @@ export default function Carousel() {
       >
         <SwiperSlide>
           <img
+            className="imgbot"
             src="https://swiperjs.com/demos/images/nature-1.jpg"
             alt="nature"
           />
         </SwiperSlide>
         <SwiperSlide>
           <img
+            className="imgbot"
             src="https://swiperjs.com/demos/images/nature-2.jpg"
             alt="nature"
           />
         </SwiperSlide>
         <SwiperSlide>
           <img
+            className="imgbot"
             src="https://swiperjs.com/demos/images/nature-3.jpg"
             alt="nature"
           />
         </SwiperSlide>
         <SwiperSlide>
           <img
+            className="imgbot"
             src="https://swiperjs.com/demos/images/nature-4.jpg"
             alt="nature"
           />
         </SwiperSlide>
         <SwiperSlide>
           <img
+            className="imgbot"
             src="https://swiperjs.com/demos/images/nature-5.jpg"
             alt="nature"
           />
         </SwiperSlide>
         <SwiperSlide>
           <img
+            className="imgbot"
             src="https://swiperjs.com/demos/images/nature-6.jpg"
             alt="nature"
           />
         </SwiperSlide>
         <SwiperSlide>
           <img
+            className="imgbot"
             src="https://swiperjs.com/demos/images/nature-7.jpg"
             alt="nature"
           />
         </SwiperSlide>
         <SwiperSlide>
           <img
+            className="imgbot"
             src="https://swiperjs.com/demos/images/nature-8.jpg"
             alt="nature"
           />
         </SwiperSlide>
         <SwiperSlide>
           <img
+            className="imgbot"
             src="https://swiperjs.com/demos/images/nature-9.jpg"
             alt="nature"
           />
         </SwiperSlide>
         <SwiperSlide>
           <img
+            className="imgbot"
             src="https://swiperjs.com/demos/images/nature-10.jpg"
             alt="nature"
           />
