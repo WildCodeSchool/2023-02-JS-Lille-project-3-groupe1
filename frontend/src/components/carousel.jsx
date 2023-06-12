c146fc680a66168996173dc376084010b3af4aa7
Michael_McGrath <corecreate0@gmail.com>
2023-06-12 14:45:51 +0200

    mapped -check

--- frontend/src/components/carousel.jsx
+++ frontend/src/components/carousel.jsx
@@ -7,15 +7,16 @@ import "swiper/scss/navigation";
 import "swiper/scss/thumbs";
 import "./carousel.scss";
 import { Swiper, SwiperSlide } from "swiper/react"; // eslint-disable-line
-
 // import required modules
 import { FreeMode, Navigation, Thumbs } from "swiper"; // eslint-disable-line
-import useineBelAir from "../assets/image/UsineBelAir_40FI78.jpg";
-import treviseCannes from "../assets/image/trevise-cannes_1861.jpg";
+// import useineBelAir from "../assets/image/UsineBelAir_40FI78.jpg";
+// import treviseCannes from "../assets/image/trevise-cannes_1861.jpg";
+import List from "../assets/Liste_oeuvres.json";
 
 export default function Carousel() {
   const [thumbsSwiper, setThumbsSwiper] = useState(null);
-
+  const allPics = List;
+  //   console.log(allPics);
   return (
     <div className="swiper-container">
       <Swiper
@@ -32,60 +33,62 @@ export default function Carousel() {
         modules={[FreeMode, Navigation, Thumbs]}
         className="mySwiper2"
       >
-        <SwiperSlide>
-          <img src={useineBelAir} alt="nature" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={treviseCannes} alt="nature" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img
-            src="https://swiperjs.com/demos/images/nature-3.jpg"
-            alt="nature"
-          />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img
-            src="https://swiperjs.com/demos/images/nature-4.jpg"
-            alt="nature"
-          />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img
-            src="https://swiperjs.com/demos/images/nature-5.jpg"
-            alt="nature"
-          />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img
-            src="https://swiperjs.com/demos/images/nature-6.jpg"
-            alt="nature"
-          />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img
-            src="https://swiperjs.com/demos/images/nature-7.jpg"
-            alt="nature"
-          />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img
-            src="https://swiperjs.com/demos/images/nature-8.jpg"
-            alt="nature"
-          />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img
-            src="https://swiperjs.com/demos/images/nature-9.jpg"
-            alt="nature"
-          />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img
-            src="https://swiperjs.com/demos/images/nature-10.jpg"
-            alt="nature"
-          />
-        </SwiperSlide>
+        {allPics.map((images) => (
+          <SwiperSlide>
+            <img key={images.ADR} src={images.link} alt="nature" />
+          </SwiperSlide>
+          // <SwiperSlide>
+          //   <img src={treviseCannes} alt="nature" />
+          // </SwiperSlide>
+          // <SwiperSlide>
+          //   <img
+          //     src="https://swiperjs.com/demos/images/nature-3.jpg"
+          //     alt="nature"
+          //   />
+          // </SwiperSlide>
+          // <SwiperSlide>
+          //   <img
+          //     src="https://swiperjs.com/demos/images/nature-4.jpg"
+          //     alt="nature"
+          //   />
+          // </SwiperSlide>
+          // <SwiperSlide>
+          //   <img
+          //     src="https://swiperjs.com/demos/images/nature-5.jpg"
+          //     alt="nature"
+          //   />
+          // </SwiperSlide>
+          // <SwiperSlide>
+          //   <img
+          //     src="https://swiperjs.com/demos/images/nature-6.jpg"
+          //     alt="nature"
+          //   />
+          // </SwiperSlide>
+          // <SwiperSlide>
+          //   <img
+          //     src="https://swiperjs.com/demos/images/nature-7.jpg"
+          //     alt="nature"
+          //   />
+          // </SwiperSlide>
+          // <SwiperSlide>
+          //   <img
+          //     src="https://swiperjs.com/demos/images/nature-8.jpg"
+          //     alt="nature"
+          //   />
+          // </SwiperSlide>
+          // <SwiperSlide>
+          //   <img
+          //     src="https://swiperjs.com/demos/images/nature-9.jpg"
+          //     alt="nature"
+          //   />
+          // </SwiperSlide>
+          // <SwiperSlide>
+          //   <img
+          //     src="https://swiperjs.com/demos/images/nature-10.jpg"
+          //     alt="nature"
+          //   />
+          // </SwiperSlide>
+        ))}
       </Swiper>
       <Swiper
         onSwiper={setThumbsSwiper}
@@ -97,68 +100,12 @@ export default function Carousel() {
         modules={[FreeMode, Navigation, Thumbs]}
         className="mySwiper"
       >
-        <SwiperSlide>
-          <img className="imgbot" src={useineBelAir} alt="nature" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img className="imgbot" src={treviseCannes} alt="nature" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img
-            className="imgbot"
-            src="https://swiperjs.com/demos/images/nature-3.jpg"
-            alt="nature"
-          />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img
-            className="imgbot"
-            src="https://swiperjs.com/demos/images/nature-4.jpg"
-            alt="nature"
-          />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img
-            className="imgbot"
-            src="https://swiperjs.com/demos/images/nature-5.jpg"
-            alt="nature"
-          />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img
-            className="imgbot"
-            src="https://swiperjs.com/demos/images/nature-6.jpg"
-            alt="nature"
-          />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img
-            className="imgbot"
-            src="https://swiperjs.com/demos/images/nature-7.jpg"
-            alt="nature"
-          />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img
-            className="imgbot"
-            src="https://swiperjs.com/demos/images/nature-8.jpg"
-            alt="nature"
-          />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img
-            className="imgbot"
-            src="https://swiperjs.com/demos/images/nature-9.jpg"
-            alt="nature"
-          />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img
-            className="imgbot"
-            src="https://swiperjs.com/demos/images/nature-10.jpg"
-            alt="nature"
-          />
-        </SwiperSlide>
+        {allPics.map((images) => (
+          <SwiperSlide>
+            <img key={images.ADR} src={images.link} alt="nature" />
+          </SwiperSlide>
+          //
+        ))}
       </Swiper>
     </div>
   );
