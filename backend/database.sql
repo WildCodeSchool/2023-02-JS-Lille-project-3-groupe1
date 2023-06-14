e18a56d6bddc038fd517731118322f19b6e3a982
Ambre <henno.ambre@gmail.com>
2023-06-14 10:46:06 +0200

    Add background image

--- backend/database.sql
+++ backend/database.sql
@@ -1,6 +1,145 @@
-CREATE TABLE item (
-  id int(11) UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  title varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-INSERT INTO item (title) VALUES ('Stuff'), ('Doodads');
+-- MySQL dump 10.13  Distrib 8.0.33, for Win64 (x86_64)
+--
+-- Host: localhost    Database: afac
+-- ------------------------------------------------------
+-- Server version	8.0.33
+​
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+​
+--
+-- Table structure for table `artworks`
+--
+​
+DROP TABLE IF EXISTS `artworks`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `artworks` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `url` mediumtext NOT NULL,
+  `Titre complet de l'œuvre` varchar(90) NOT NULL,
+  `Titre résumé de l'œuvre` varchar(90) NOT NULL,
+  `Commentaire de AFAC974` mediumtext NOT NULL,
+  `Dimensions` varchar(45) NOT NULL,
+  `Technique` varchar(45) NOT NULL,
+  `author` varchar(45) NOT NULL,
+  `Année de réalisation` date NOT NULL,
+  `author_id` int NOT NULL,
+  `Catégorie` varchar(45) NOT NULL,
+  `Article lié` mediumtext,
+  PRIMARY KEY (`id`),
+  KEY `fk_artworks_author_idx` (`author_id`),
+  CONSTRAINT `fk_artworks_author` FOREIGN KEY (`author_id`) REFERENCES `author` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+​
+--
+-- Dumping data for table `artworks`
+--
+​
+LOCK TABLES `artworks` WRITE;
+/*!40000 ALTER TABLE `artworks` DISABLE KEYS */;
+/*!40000 ALTER TABLE `artworks` ENABLE KEYS */;
+UNLOCK TABLES;
+​
+--
+-- Table structure for table `author`
+--
+​
+DROP TABLE IF EXISTS `author`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `author` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `firstname` varchar(45) NOT NULL,
+  `lastname` varchar(45) NOT NULL,
+  `dob` date NOT NULL,
+  `dod` date DEFAULT NULL,
+  `nationality` varchar(45) NOT NULL,
+  `description` mediumtext NOT NULL,
+  `avatarUrl` mediumtext NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+​
+--
+-- Dumping data for table `author`
+--
+​
+LOCK TABLES `author` WRITE;
+/*!40000 ALTER TABLE `author` DISABLE KEYS */;
+/*!40000 ALTER TABLE `author` ENABLE KEYS */;
+UNLOCK TABLES;
+​
+--
+-- Table structure for table `favori`
+--
+​
+DROP TABLE IF EXISTS `favori`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `favori` (
+  `artworks_id` int NOT NULL,
+  `user_id` int NOT NULL,
+  PRIMARY KEY (`artworks_id`,`user_id`),
+  KEY `fk_artworks_has_user_user1_idx` (`user_id`),
+  KEY `fk_artworks_has_user_artworks1_idx` (`artworks_id`),
+  CONSTRAINT `fk_artworks_has_user_artworks1` FOREIGN KEY (`artworks_id`) REFERENCES `artworks` (`id`),
+  CONSTRAINT `fk_artworks_has_user_user1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+​
+--
+-- Dumping data for table `favori`
+--
+​
+LOCK TABLES `favori` WRITE;
+/*!40000 ALTER TABLE `favori` DISABLE KEYS */;
+/*!40000 ALTER TABLE `favori` ENABLE KEYS */;
+UNLOCK TABLES;
+​
+--
+-- Table structure for table `user`
+--
+​
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `mail` varchar(90) NOT NULL,
+  `hashedPassword` varchar(45) NOT NULL,
+  `admin` tinyint DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `mail_UNIQUE` (`mail`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+​
+--
+-- Dumping data for table `user`
+--
+​
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+​
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+​
+-- Dump completed on 2023-06-14  9:52:46
