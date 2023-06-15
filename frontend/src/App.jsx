3764cbc4d58454d3c76704abbeb15e77f85f551d
Ambre <henno.ambre@gmail.com>
2023-06-15 12:37:22 +0200

    added navbar on home page with modification of measurement

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,17 @@
 import "./App.scss";
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./assets/components/Navbar/Navbar";
+import HomePage from "./assets/components/HomePage/HomePage";
 
 export default function App() {
   return (
-    <>
+    <div className="app_container">
       <Navbar />
-
       <Routes>
-        <Route path="/" element="" />
+        <Route path="/" element={<HomePage />} />
         <Route path="/galerie" element="" />
         <Route path="/apropos" element="" />
       </Routes>
-    </>
+    </div>
   );
 }
