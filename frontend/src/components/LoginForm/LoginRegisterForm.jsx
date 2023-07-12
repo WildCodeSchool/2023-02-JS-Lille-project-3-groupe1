96ccaa9c26b8a117f37a5a22e705bfd35d3698bb
wilder <henno.ambre@gmail.com>
2023-07-12 14:34:50 +0200

    more comments

--- frontend/src/components/LoginForm/LoginRegisterForm.jsx
+++ frontend/src/components/LoginForm/LoginRegisterForm.jsx
@@ -8,14 +8,17 @@ function LoginRegisterForm() {
         <a href="Home">AFAC 974</a>
       </p>
       <form>
+        {/* Champ de saisie de l'email */}
         <div className="user-box">
           <input required name="" type="text" />
           <label>Email</label>
         </div>
+        {/* Champ de saisie du mot de passe */}
         <div className="user-box">
           <input required name="" type="password" />
           <label>Mot de passe</label>
         </div>
+        {/* Bouton de connexion */}
         <div className="connexion-button">
           <a href="#">
             <span></span>
@@ -26,12 +29,14 @@ function LoginRegisterForm() {
           </a>
         </div>
       </form>
+      {/* Lien pour réinitialiser le mot de passe */}
       <p className="forgot-password">
         <a href="" className="forgot-password">
           Mot de passe oublié ?
         </a>
       </p>
 
+      {/* Lien pour créer un compte */}
       <p className="account-sign-up">
         Vous n'avez pas de compte ?{" "}
         <a href="" className="suscribe">
