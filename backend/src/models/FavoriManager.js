3eef9efc85adec99251bd8f50613ca71a314f61f
Michael_McGrath <corecreate0@gmail.com>
2023-07-13 11:03:43 +0200

    working fav function added + css implemented, rotated pics correctly in db

--- backend/src/models/FavoriManager.js
+++ backend/src/models/FavoriManager.js
@@ -6,7 +6,6 @@ class FavoriManager extends AbstractManager {
   }
 
   insert(favori) {
-    console.log("fav in insert => ", favori);
     return this.database.query(
       `insert into ${this.table} (artworks_id, user_id) values (?, ?)`,
       [favori.artworks_id, favori.user_id]
@@ -25,34 +24,6 @@ class FavoriManager extends AbstractManager {
       `update ${this.table} set artworks_id = ? where user_id = ?`,
       [favori.artworks_id, favori.user_id]
     );
-  }
-
-  getFavoriteArtworks(userId, callback) {
-    const sql = `
-      SELECT artworks.*
-      FROM artworks
-      INNER JOIN favori ON artworks.id = favori.artworks_id
-      WHERE favori.user_id = ?
-    `;
-    const params = [userId];
-
-    // Send the SQL query to the database
-    this.database.query(sql, params, (error, results) => {
-      if (error) {
-        callback(error);
-        return;
-      }
-
-      // Map the results to an array of JavaScript objects
-      const favoriteArtworks = results.map((row) => ({
-        id: row.id,
-        title: row.title,
-        image: row.image,
-        // Add more properties as needed
-      }));
-
-      callback(null, favoriteArtworks);
-    });
   }
 }
 
